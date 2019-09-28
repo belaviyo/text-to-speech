16a79b9111f7542f9965d219589f5f62bf4f97b0
Joe Ertaba <belaviyo@gmail.com>
2019-09-28 12:16:43 +0000

    isolating the controls from the page UI

--- example/index.js
+++ example/index.js
@@ -1,23 +1,25 @@
 /* globals TTS */
 'use strict';
 
-var tts = new TTS();
-tts.feed(document.getElementById('root'));
-tts.attach(document.getElementById('tts'));
+document.addEventListener('DOMContentLoaded', async () => {
+  const tts = new TTS();
+  tts.feed(document.getElementById('root'));
+  await tts.attach(document.getElementById('tts'));
 
-//tts.on('section', n => console.log('section ends', n));
-// tts.on('end', () => console.log('playing session ended'));
-//tts.on('status', s => console.log('status', s));
+  // tts.on('section', n => console.log('section ends', n));
+  // tts.on('end', () => console.log('playing session ended'));
+  // tts.on('status', s => console.log('status', s));
+  // tts.on('idle', s => console.log('status', s, 'all sections are finished or the users manually stopped the player'));
+  // tts.ready().then(() => console.log('ready'));
 
-// tts.ready().then(() => console.log('ready'));
-
-document.getElementById('start').addEventListener('click', () => {
-  tts.create();
-  tts.start();
+  document.getElementById('start').addEventListener('click', () => {
+    tts.create();
+    tts.start();
+  });
+  document.getElementById('stop').addEventListener('click', () => tts.stop());
+  document.getElementById('resume').addEventListener('click', () => tts.resume());
+  document.getElementById('pause').addEventListener('click', () => tts.pause());
+  document.getElementById('next').addEventListener('click', () => tts.navigate('forward'));
+  document.getElementById('previous').addEventListener('click', () => tts.navigate('backward'));
+  document.getElementById('reset').addEventListener('click', () => tts.reset());
 });
-document.getElementById('stop').addEventListener('click', () => tts.stop());
-document.getElementById('resume').addEventListener('click', () => tts.resume());
-document.getElementById('pause').addEventListener('click', () => tts.pause());
-document.getElementById('next').addEventListener('click', () => tts.navigate('forward'));
-document.getElementById('previous').addEventListener('click', () => tts.navigate('backward'));
-document.getElementById('reset').addEventListener('click', () => tts.reset());
